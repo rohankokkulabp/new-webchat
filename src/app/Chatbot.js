7f626bd983db5754148e13f89915045b30982a94
rohankokkulabp <rohan.kokkula@botpress.com>
2023-03-17 18:04:15 +0530

    dummy bot

--- src/app/Chatbot.js
+++ src/app/Chatbot.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 
 const Chatbot = () => {
@@ -13,21 +13,22 @@ const Chatbot = () => {
       script.addEventListener("load", () => {
         try {
           window.botpressWebChat.init({
-            botId: "9f6abe0a-7145-4cd2-81d0-3f60e771c341",
+            botId: "09d8ea5b-2781-4fe4-a0b5-d75798b24c50",
             hostUrl: "https://cdn.botpress.cloud/webchat/v0",
             messagingUrl: "https://messaging.botpress.cloud",
-            clientId: "9f6abe0a-7145-4cd2-81d0-3f60e771c341",
+            clientId: "09d8ea5b-2781-4fe4-a0b5-d75798b24c50",
             botName: "ChatGPT x Botpress",
             showPoweredBy: true,
             containerWidth: "100%25",
             layoutWidth: "100%25",
             showCloseButton: false,
-            botConversationDescription: "A simple chatbot that demonstrates integration of ChatGPT, with Botpress",
+            botConversationDescription:
+              "A simple chatbot that demonstrates integration of ChatGPT, with Botpress",
             hideWidget: true,
             disableAnimations: true,
             closeOnEscape: false,
             showConversationsButton: true,
-            avatarURL: 'https://app.botpress.cloud/chatbots/abstract-26.svg',
+            avatarURL: "https://app.botpress.cloud/chatbots/abstract-26.svg",
             enableTranscriptDownload: false,
             stylesheet: "http://localhost:3000/chat.css",
           });
@@ -36,12 +37,12 @@ const Chatbot = () => {
             function () {
               window.botpressWebChat.sendEvent({ type: "show" });
               window.botpressWebChat.sendEvent({
-                type: 'proactive-trigger',
-                channel: 'web',
+                type: "proactive-trigger",
+                channel: "web",
                 payload: {
-                  text: 'Hi, I am Pro active'
-                }
-              })
+                  text: "Hi, I am Pro active",
+                },
+              });
             },
             ["LIFECYCLE.LOADED"]
           );
@@ -67,7 +68,7 @@ const ChatbotWithRectangle = () => {
           height: "100%",
           width: "500px",
           backgroundColor: "gray",
-          marginRight: "10px"
+          marginRight: "10px",
         }}
       />
       <div style={{ flex: 1 }}>
